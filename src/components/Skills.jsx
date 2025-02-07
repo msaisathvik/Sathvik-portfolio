223a8351e3ee19b7440d9d3781af707af7190017
SATHVIK <mattasathvik@gmail.com>
2025-02-07 18:05:29 +0530

    updated files

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,22 +8,22 @@ const Skills = () => {
         <fieldset className='border'>
           <legend className='text-center text-2xl'>Languages</legend>
           <div className='flex flex-row  w-[70vw] justify-around p-5 mt-5'>
-            <p><img src={`/assets/C.png`} alt="C Language" /></p>
-            <p><img src={`/assets/c-plus-plus-programming-language-icon.png`} alt="C++ Language" /></p>
-            <p><img src={`/assets/python-programming-language-icon.png`} alt="Python Language" /></p>
-            <p><img src={`/assets/javascript-programming-language-icon.png`} alt="JavaScript Language" /></p>
-            <p><img src={`/assets/html-icon.png`} alt="HTML" /></p>
-            <p><img src={`/assets/css-icon.png`} alt="CSS" /></p>
+            <p><img src={`/assets/C.PNG`} alt="C Language" /></p>
+            <p><img src={`/assets/c-plus-plus-programming-language-icon.PNG`} alt="C++ Language" /></p>
+            <p><img src={`/assets/python-programming-language-icon.PNG`} alt="Python Language" /></p>
+            <p><img src={`/assets/javascript-programming-language-icon.PNG`} alt="JavaScript Language" /></p>
+            <p><img src={`/assets/html-icon.PNG`} alt="HTML" /></p>
+            <p><img src={`/assets/css-icon.PNG`} alt="CSS" /></p>
           </div>
           
         </fieldset>
         <fieldset className='border'>
           <legend className='text-center text-2xl'>Technologies</legend>
           <div className='flex flex-row  w-[70vw] justify-around p-5 mt-5'>
-            <p><img src="/assets/node-js-icon.png" alt="Node js" /></p>
-            <p><img src="/assets/react-js-icon.png" alt="React js" /></p>
-            <p><img src="/assets/express-js-icon.png" alt="Express js" /></p>
-            <p><img src="/assets/postgresql-icon.png" alt="PostgreSQL" /></p>
+            <p><img src="/assets/node-js-icon.PNG" alt="Node js" /></p>
+            <p><img src="/assets/react-js-icon.PNG" alt="React js" /></p>
+            <p><img src="/assets/express-js-icon.PNG" alt="Express js" /></p>
+            <p><img src="/assets/postgresql-icon.PNG" alt="PostgreSQL" /></p>
           </div>
           
         </fieldset>
